4589e7dfe80b4308be087e05e016e281c1f68f92
Idriss Chefai <namidid2.0@gmail.com>
2024-09-22 20:34:28 +0100

    Prosit 1 Termine

--- ZooManagement.java
+++ ZooManagement.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ZooManagement {
-    public static void main (String args){
-        
+    public static void main(String[] args) {
+        int nbrCages = 20;
+        String zooName = "my zoo";
+        Scanner scanner = new Scanner(System.in);
+
+        // Saisie zooName
+        System.out.println("Donner le nom du zoo");
+        zooName = scanner.nextLine();
+
+        // Saisie nbrCages
+        System.out.println("Donner le nombre de cages");
+        nbrCages = scanner.nextInt();
+
+        System.out.println(zooName + " comporte " + nbrCages + " cages");
+
+        // Fermeture du scanner
+        scanner.close();
     }
 }
